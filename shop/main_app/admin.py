8663c2b8fecf5df6e56eae92254a1c88b877a5ed
maxizorin <sz5s4q42md>
2021-09-03 13:53:40 +0300

    Сделал ограничения на минимальный и максимальный размер изображений. Улучшил логику админки, добавил метод главной модели, чтобы ограничения были не только через админку.

--- shop/main_app/admin.py
+++ shop/main_app/admin.py
@@ -1,6 +1,31 @@
 from django import forms
+from django.forms import ModelForm, ValidationError
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import *
+from PIL import Image
+
+
+class ParfumAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size:14px;">Минимальное разрешение изображения {}x{}</span>'.format(
+                *Product.MIN_RESOLUTION
+            )
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение изображения больше минимального')
+        return image
 
 
 class ParfumCategoryChoiceFields(admin.ModelAdmin):
@@ -9,6 +34,9 @@ class ParfumCategoryChoiceFields(admin.ModelAdmin):
 
 
 class ParfumAdmin(admin.ModelAdmin):
+
+    form = ParfumAdminForm
+
     # Класс для исключения других категорий при добавлении товара
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'categorys':
--- shop/main_app/models.py
+++ shop/main_app/models.py
@@ -1,9 +1,19 @@
+from PIL import Image
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
+
+
+class MinResolutionErrorException(Exception):
+   pass
+
+
+class MaxResolutionErrorException(Exception):
+   pass
 
 
 class LatestProductManager:
@@ -52,6 +62,9 @@ class ParfumVolume(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (300, 300)
+    MAX_RESOLUTION = (4000, 4000)
+
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     brand = models.CharField(max_length=255, verbose_name='Бренд', blank=True)
     title = models.CharField(max_length=255, verbose_name='Название')
@@ -65,6 +78,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorException('Разрешение изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorException('Разрешение изображения больше минимального')
+        return image
 
     class Meta:
         abstract = True
